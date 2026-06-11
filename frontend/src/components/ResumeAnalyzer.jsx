8db27f3919f337a5e4c07aff3862a33c664adf4d
Deebiga <deebigasubramaniyan@gmail.com>
2026-06-11 16:40:34 +0530

    starting

---
+++ frontend/src/components/ResumeAnalyzer.jsx
@@ -0,0 +1,36 @@
+function ResumeAnalyzer(){
+
+return(
+
+<div className="card">
+
+<h2>Resume Analyzer</h2>
+
+
+<div className="upload">
+
+Upload Resume
+
+<input type="file"/>
+
+</div>
+
+
+<p>
+Matched Skills:
+</p>
+
+
+<span>Python</span>
+<span>AI</span>
+<span>ML</span>
+<span>React</span>
+
+
+</div>
+
+)
+
+}
+
+export default ResumeAnalyzer;
