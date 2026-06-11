3ea02731742c78142d32e7a1d0c6a5406930b899
Deebiga <deebigasubramaniyan@gmail.com>
2026-06-11 17:13:16 +0530

    sidebar

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,52 @@
+export default function Sidebar(){
+
+return(
+
+<div className="sidebar">
+
+
+<h2>
+✨ AI Interviewer
+</h2>
+
+
+<div className="active">
+🏠 Home
+</div>
+
+
+<div>
+🎤 Mock Interview
+</div>
+
+
+<div>
+📄 Resume Analyzer
+</div>
+
+
+<div>
+📚 Practice Topics
+</div>
+
+
+<div>
+📊 Interview History
+</div>
+
+
+<div>
+🤖 AI Feedback
+</div>
+
+
+<div>
+⚙ Settings
+</div>
+
+
+</div>
+
+)
+
+}
