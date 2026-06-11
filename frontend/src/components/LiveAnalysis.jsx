49f53d9fa709245c1e205a0bf425fd2de4156ec8
Deebiga <deebigasubramaniyan@gmail.com>
2026-06-11 17:13:44 +0530

    analysis

---
+++ frontend/src/components/LiveAnalysis.jsx
@@ -0,0 +1,71 @@
+export default function LiveAnalysis(){
+
+return(
+
+<div className="analysisPanel">
+
+
+<h2>
+✨ Live Analysis
+</h2>
+
+
+<div className="score">
+
+Confidence
+<h1>78%</h1>
+
+</div>
+
+
+<div className="score">
+
+Clarity
+<h1>82%</h1>
+
+</div>
+
+
+
+<div className="score">
+
+Communication
+<h1>85%</h1>
+
+</div>
+
+
+
+<div className="score">
+
+Technical
+<h1>75%</h1>
+
+</div>
+
+
+
+<div className="wave">
+
+〰〰〰〰〰〰
+
+</div>
+
+
+
+<h3>
+Current Question
+</h3>
+
+
+<p>
+Explain supervised and unsupervised learning.
+</p>
+
+
+</div>
+
+
+)
+
+}
