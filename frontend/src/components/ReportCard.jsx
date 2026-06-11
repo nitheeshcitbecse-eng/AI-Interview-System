434069bfd4b1801d4a9869e1743b3d3a9e224449
Deebiga <deebigasubramaniyan@gmail.com>
2026-06-11 16:41:56 +0530

    starting report

---
+++ frontend/src/components/ReportCard.jsx
@@ -0,0 +1,37 @@
+function ReportCard(){
+
+return(
+
+<div className="card">
+
+<h2>Interview Report</h2>
+
+
+<h1>
+87/100
+</h1>
+
+
+<p>
+Communication ⭐⭐⭐⭐⭐
+</p>
+
+<p>
+Technical ⭐⭐⭐⭐
+</p>
+
+<p>
+Improve:
+Answer structure,
+confidence,
+real examples
+</p>
+
+
+</div>
+
+)
+
+}
+
+export default ReportCard;
