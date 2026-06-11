9b1bbf673a2ef59ddc034ad5d364b2286a7893d4
Deebiga <deebigasubramaniyan@gmail.com>
2026-06-11 16:42:46 +0530

    working setting

---
+++ frontend/src/components/SettingsPanel.jsx
@@ -0,0 +1,28 @@
+function SettingsPanel(){
+
+return(
+
+<div className="card">
+
+
+<h2>Settings</h2>
+
+
+<p>Voice : Female</p>
+
+<p>
+Speech Speed : Normal
+</p>
+
+<p>
+Camera : ON
+</p>
+
+
+</div>
+
+)
+
+}
+
+export default SettingsPanel;
